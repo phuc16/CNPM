3cfcce8e935efea26070bef5976d815a28818245
dieuaibk19 <nguyendieuai082001@gmail.com>
2021-10-25 00:18:25 +0700

    khoitao

---
+++ clerkview/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h2>Welcome to Clerkview </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;

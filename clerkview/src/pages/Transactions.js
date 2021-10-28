3e94f089364531f1c1639476a6d10e5f5e9681cf
remsokawaii1 <remsokawaii1@gmail.com>
2021-10-28 08:25:53 +0700

    Pending Order Detail with responsive

---
+++ clerkview/src/pages/Transactions.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Transactions extends Component {
+    render(){
+        return(
+            <h2>Transactions Pages</h2>
+        );
+    };
+}
+
+export default Transactions;

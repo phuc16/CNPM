27f0da419f96a453dce928bad434ac3496468a0c
dieuaibk19 <nguyendieuai082001@gmail.com>
2021-11-12 23:30:55 +0700

    update

--- clerkview/src/components/NavBar.js
+++ clerkview/src/components/NavBar.js
@@ -1,9 +1,9 @@
 import "./NavBar.css";
 
-export default function navBar({ Title }) {
+export default function navBar({Title}) {
   return (
     <div className="navbar">
-      <h1 className="noselect">{Title}</h1>
+      <h1 className="insidetext">{Title}</h1>
     </div>
   );
 }

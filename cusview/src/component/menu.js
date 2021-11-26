49f2b3449c556266c61a289ee2243676059e9108
NoKnight41 <tdmdo2612@gmail.com>
2021-11-26 23:15:08 +0700

    Just CSS

--- cusview/src/component/menu.js
+++ cusview/src/component/menu.js
@@ -12,7 +12,6 @@ export default function Menu(props){
           </div>
           <div class="content-product">
               <h3 class="content-product-h3">{Name}</h3>
-              <h3 class="content-product-h3">{description}</h3>
 
               <div class="content-product-deltals">
                   <div class="price">
